6ce852ea91d0624e6f3f935d23c9fd310d1f39bb
Oktavian Bayu Widodo <oktavianbayuwidodo7@gmail.com>
2024-01-04 11:39:48 +0700

    get all list and detail
    get all list and detail : validate

--- django_json_schema_fe/app/validate.py
+++ django_json_schema_fe/app/validate.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
 import requests
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework import status
 
 def index(request):
     api_url = 'http://localhost:8000/allData/'
@@ -10,5 +13,23 @@ def index(request):
 
     return render(request, "validate/index.html", context)
 
-def insert_data(request):
-    return render(request, "generate/index.html")
+def detail(request, url_path):
+    print(url_path)
+    api_url = f'http://127.0.0.1:8000/getData/?url_path=/{url_path}'
+    response = requests.get(api_url)
+    data = response.json()
+
+    # Menyediakan data ke template
+    context = {'data': data}
+
+    return render(request, 'validate/detail.html', context)
+
+@api_view(['POST'])
+def validateJson(request):
+    api_url = 'http://127.0.0.1:8000/validate_json/'
+    response = requests.get(api_url)
+    data = response.json()
+
+    context = {'data': data}
+
+    return render(request, 'validate/detail.html', context)
