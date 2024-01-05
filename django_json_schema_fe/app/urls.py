08f33dabbc3d01a4272ca09b4aae8bff4a00e4e0
Oktavian Bayu Widodo <oktavianbayuwidodo7@gmail.com>
2024-01-05 09:25:22 +0700

    validate json
    validate json and json schema

--- django_json_schema_fe/app/urls.py
+++ django_json_schema_fe/app/urls.py
@@ -10,7 +10,7 @@ urlpatterns = [
     path('insert', generate.insert_data, name="insert_data"),
     path('validate', validate.index, name="index_validate"),
     path('detail/<str:url_path>', validate.detail, name='detail_view'),
-    path('/validateJson', validate.validateJson, name='validate_json')
+    path('validateJson', validate.validateJson, name='validate_json')
     # path('generate_json_schema', generate.generate_json_schema, name="generate_json_schema")
     # validate views
 
--- django_json_schema_fe/app/validate.py
+++ django_json_schema_fe/app/validate.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 import requests
+import json
+import re
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework import status
@@ -26,10 +28,33 @@ def detail(request, url_path):
 
 @api_view(['POST'])
 def validateJson(request):
+    json_schema = request.data.get('json_schema')
+    json_schema_remove_quote = remove_quotes_around_keys(json_schema)
+    url_path = request.data.get('url_path')
+    json_string = request.data.get('json_string')
+
     api_url = 'http://127.0.0.1:8000/validate_json/'
-    response = requests.get(api_url)
-    data = response.json()
+    payload = {
+        'json_schema': json_schema,
+        'url_path': url_path,
+        'json_string': json_string,
+    }
 
-    context = {'data': data}
+    response = requests.post(api_url, json=payload)
 
-    return render(request, 'validate/detail.html', context)
+    return Response(response.json())
+
+def remove_quotes_around_keys(json_str):
+    matches = re.findall(r'"(\w+)":', json_str)
+
+    for match in matches:
+        json_str = json_str.replace(f'"{match}":', f'{match}:')
+
+    return json_str
+
+def transform_json_format(json_str):
+    json_str = re.sub(r'(\w+):', r'\1: ', json_str)
+
+    json_str = re.sub(r'"(\w+)":', r'\1: ', json_str)
+
+    return json_str
